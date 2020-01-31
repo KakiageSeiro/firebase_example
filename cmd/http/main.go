27b073fd7b3ed4a8724dd057ca8355056fd9cc5c
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2020-01-31 21:14:26 +0900

    mainからローカルでWebサーバーを起動できるように修正

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	helloworld "gitlab.com/mc_go/firebase_example"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloworld.HelloHTTP)
+	http.ListenAndServe(":8080", nil)
+}
--- hello_http.go
+++ hello_http.go
@@ -13,12 +13,14 @@ func HelloHTTP(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Name string `json:"name"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World!")
+		fmt.Fprint(w, "jsonがデコードできなかったよHello, World!")
 		return
 	}
+
 	if d.Name == "" {
-		fmt.Fprint(w, "Hello, World!")
+		fmt.Fprint(w, "名前が空文字だよHello, World!")
 		return
 	}
 	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
